c3fa33de94ead672dbd4ce2ca57cbe6d7a752753
y0m0 <lixi.daniele@gmail.com>
2017-04-15 22:23:18 +0200

    ex 1:1-7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,15 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  result = ""
+  string.size.times do |i|
+    if string[i].downcase == "r" && i < string.size - 1
+      result += string[i + 1]
+    end
+  end
+  result
 end
