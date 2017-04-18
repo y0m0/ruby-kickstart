6980202cf84d7f9622b4cce6e6e566f86988dd66
y0m0 <lixi.daniele@gmail.com>
2017-04-19 01:30:44 +0200

    ex 3:1

--- session3/challenge/1_blocks.rb
+++ session3/challenge/1_blocks.rb
@@ -3,3 +3,10 @@
 #
 # reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
 
+def reverse_map(*args, &block)
+  res = []
+  (args.length - 1).downto(0) do |i|
+    res << block.call(args[i])
+  end
+  res
+end
