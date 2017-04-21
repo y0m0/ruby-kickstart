8d628a86591ae0f03363d61e0b30d64017ad1b23
y0m0 <lixi.daniele@gmail.com>
2017-04-22 00:08:13 +0200

    ex3:10

--- session3/challenge/10_hashes.rb
+++ session3/challenge/10_hashes.rb
@@ -43,7 +43,20 @@ def pathify(structure)
   tree
 end
 
-pathify ({'usr' => {'bin' => ['ruby'] }}) # => "/usr"
+## solution
 
-# >> usr: is a hash: true
-# >> bin: is a hash: false
+def pathify(paths=Hash.new)
+  # base step
+  return paths.map { |path| '/' + path } if paths.is_a? Array
+
+  # recursive step
+  to_return = []
+  paths.each do |parent_path, child_dirs|
+    parent_path = '/' + parent_path         # paths begin with a /
+    child_paths = pathify child_dirs        # convert child directories to paths
+    child_paths.each do |child_path|        # join each child path to it's parent path
+      to_return << (parent_path + child_path)
+    end
+  end
+  to_return
+end
