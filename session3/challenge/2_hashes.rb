cab34b15d9fc90c58db38d7bfcde6cd0d73d0c37
y0m0 <lixi.daniele@gmail.com>
2017-04-24 00:35:33 +0200

    ex3:2

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -9,14 +9,11 @@
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
 
-
-def staircase(n)
+def staircase(num)
   hash = {}
-  1.upto(n) do |number|
-    next if number.even?
-    all = Array.new(number) {|i| i + 1}
-    evens = all.select { |i| i.even? }
-    hash[number] = evens
+  odd_keys = (1..num).select {|n| n.odd?}
+  odd_keys.each do |key|
+    hash[key] = (1..key).select {|n| n.even?}
   end
   hash
 end
